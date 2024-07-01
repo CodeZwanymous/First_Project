da5c61d29d570d519e880432dd7d7249c9f44f04
rezwan.rizvy10@gmail.com <rezwan.rizvy10@gmail.com>
2024-07-01 19:10:17 +0600

    none

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('Hello World!');
+document.getElementById('changeButton').addEventListener('click', function() {
+    var messageElement = document.getElementById('message');
+    messageElement.textContent = "You clicked the button!";
+});
